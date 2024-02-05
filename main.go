8274401993644f691fcd750288abeaae873dff13
Loknath Dhar <loknath-dhar@pm.me>
2024-02-06 00:08:23 +0600

    Added snippetView and snippetCreate handler

--- main.go
+++ main.go
@@ -10,13 +10,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 // snippetView handler
-
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
 
 // snippetCreate handler
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
