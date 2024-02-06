226d493fd7db4f1d1d57a5383637ec64269c6a16
Loknath Dhar <loknath-dhar@pm.me>
2024-02-06 17:21:53 +0600

    santized input for id in snippetview

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +19,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // snippetView handler
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// w.Write([]byte("Display a specific snippet..."))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // snippetCreate handler
