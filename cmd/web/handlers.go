455fd179c9ffad8f753b29e6362a637ea5aa6301
Loknath Dhar <loknath-dhar@pm.me>
2024-02-07 14:58:26 +0600

    rewrote handlers.go for loading base template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,15 +14,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
 	// w.Write([]byte("Hello from Snippetbox"))
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	// err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
