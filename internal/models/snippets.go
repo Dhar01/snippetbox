0ddd1950e051fa80ced6635e20622f86ee7fa3fb
Loknath Dhar <loknath-dhar@pm.me>
2024-02-22 17:38:48 +0600

    rewrite code for Get - shortend

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -43,12 +43,22 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
 	// QueryRow() method on connection pool will execute SQL statement.
-	row := m.DB.QueryRow(stmt, id)
+	// row := m.DB.QueryRow(stmt, id)
 
 	// initialize a pointer to a new zeroed Snippet struct
 	s := &Snippet{}
 
-	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	// err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	// if err != nil {
+	// 	if errors.Is(err, sql.ErrNoRows) {
+	// 		return nil, ErrNoRecord
+	// 	} else {
+	// 		return nil, err
+	// 	}
+	// }
+
+	// rewriting above code
+	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrNoRecord
