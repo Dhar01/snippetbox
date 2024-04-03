66f95631bf2dd57671bdf8292da5260231636b20
Loknath Dhar <loknath-dhar@pm.me>
2024-04-03 18:21:13 +0600

    deleting unnecessary commented code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,20 +12,13 @@ import (
 )
 
 type snippetCreateForm struct {
-	Title   string
-	Content string
-	Expires int
-	// FieldErrors map[string]string
+	Title               string
+	Content             string
+	Expires             int
 	validator.Validator `form:"-"`
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-
-	// if r.URL.Path != "/" {
-	// 	app.notFound(w)
-	// 	return
-	// }
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -76,19 +69,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-
-	// if r.Method != http.MethodPost {
-	// 	w.Header().Set("Allow", http.MethodPost)
-	// 	app.clientError(w, http.StatusMethodNotAllowed)
-	// 	return
-	// }
-
-	// err := r.ParseForm()
-	// if err != nil {
-	// 	app.clientError(w, http.StatusBadRequest)
-	// 	return
-	// }
-
 	var form snippetCreateForm
 
 	err := app.decodePostForm(r, &form)
@@ -96,29 +76,6 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-
-	// retrieve title and content
-	// title := r.PostForm.Get("title")
-	// content := r.PostForm.Get("content")
-
-	// // dummy data
-	// title := "O snail"
-	// content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	// expires := 7
-
-	// expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-	// if err != nil {
-	// 	app.clientError(w, http.StatusBadRequest)
-	// 	return
-	// }
-
-	// // instance of SnippetCreateForm struct
-	// form := snippetCreateForm{
-	// 	Title:   r.PostForm.Get("title"),
-	// 	Content: r.PostForm.Get("content"),
-	// 	Expires: expires,
-	// 	// FieldErrors: map[string]string{},
-	// }
 
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
@@ -131,28 +88,6 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
 		return
 	}
-
-	// // title cannot be blank and not more than 100 characters long
-	// if strings.TrimSpace(form.Title) == "" {
-	// 	form.FieldErrors["title"] = "This field cannot be blank"
-	// } else if utf8.RuneCountInString(form.Title) > 100 {
-	// 	form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	// }
-	// // content cannot be blank
-	// if strings.TrimSpace(form.Content) == "" {
-	// 	form.FieldErrors["content"] = "This field cannot be blank"
-	// }
-	// // expires values should be matched
-	// if form.Expires != 11 && form.Expires != 7 && form.Expires != 365 {
-	// 	form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	// }
-	// // any errors, dump them in plain text http response
-	// if len(form.FieldErrors) > 0 {
-	// 	data := app.newTemplateData(r)
-	// 	data.Form = form
-	// 	app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
-	// 	return
-	// }
 
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
